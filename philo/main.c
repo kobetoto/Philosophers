9f36ab1d11cefeb158a36fbdb3c2f04347af2363
kobe <thodavid@student.42.fr>
2025-05-02 13:22:48 +0200

    Update main.c

--- philo/main.c
+++ philo/main.c
@@ -6,24 +6,60 @@
 /*   By: thodavid <thodavid@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/23 15:08:24 by thodavid          #+#    #+#             */
-/*   Updated: 2025/05/01 14:34:28 by thodavid         ###   ########.fr       */
+/*   Updated: 2025/05/02 13:19:25 by thodavid         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
 
+void* routine(void *arg)
+{
+	long time_to_eat = *((long *)arg);
+	// while(time_to_eat)
+	// {
+		printf("EAT\n");
+		time_to_eat--;
+	// }
+	free(arg);
+	return (NULL);
+}
+
 int	main(int ac, char **av)
 {
 	t_data	data;
-
+	pthread_t *arr_p;
 	data = ft_parsing(ac, av);
-	printf("==number of philo:%d==\n", data.number_of_philos);
-	printf("==time to die:%ld==\n", data.time_to_die);
-	printf("==time to eat:%ld==\n", data.time_to_eat);
-	printf("==time to sleep:%ld==\n", data.time_to_sleep);
-	printf("==max meal:%d==\n", data.max_meal);
+	arr_p = ft_philo_former(&data, &routine);
+
+	ft_free_data(arr_p, data.number_of_philos);
 	return (0);
 }
+
+int	ft_free_data(pthread_t *arr, int p)
+{
+	int	i;
+	
+	i = 0;
+	while (i < p)
+		{
+			if (pthread_join(arr[i], NULL) != 0) {
+				return (1);
+			}
+			i++;
+		}
+	free(arr);
+	return (0);
+}
+
+
+long get_timestamp_ms(void)
+{
+    struct timeval tv;
+    gettimeofday(&tv, NULL);
+    return (tv.tv_sec * 1000) + (tv.tv_usec / 1000);
+}
+
+
 /*
 number_of_philosophers 
 time_to_die 
