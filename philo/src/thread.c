d41d75bbc66ba38f58113cedcfb167aded0cf337
kobe <thodavid@student.42.fr>
2025-05-12 16:04:24 +0200

    Update thread.c

--- philo/src/thread.c
+++ philo/src/thread.c
@@ -6,7 +6,7 @@
 /*   By: thodavid <thodavid@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/02 09:03:48 by thodavid          #+#    #+#             */
-/*   Updated: 2025/05/04 15:00:26 by thodavid         ###   ########.fr       */
+/*   Updated: 2025/05/12 16:00:04 by thodavid         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,46 +15,34 @@
 pthread_t *ft_philo_thread(t_data *data, void *f())
 {
     int i;
-    int check_t;
-    pthread_t *philo_arr;
+    int check_thread;
+    //creation dun tableau de thread(philo)
+    pthread_t *philo_id_arr;
 
     i = 0;
-    philo_arr = malloc(sizeof(pthread_t) * data->number_of_philos);
-    if (philo_arr == NULL)
+    //allocation de memoire pour le tableau de philos
+    philo_id_arr = malloc(sizeof(pthread_t) * data->number_of_philos);
+    if (philo_id_arr == NULL)
         ft_error("philo_arr malloc failed");
+    //remplissage du tableau 
     while (i < data->number_of_philos)
     {
-        int *philo_id;
-        philo_id = malloc(sizeof(int));
-        *philo_id = i;
-        check_t = 0;
+        check_thread = 0;
         usleep(242);
-        check_t = pthread_create(&philo_arr[i], NULL, f, philo_id);
-        if (check_t != 0)
+        check_thread = pthread_create(&philo_id_arr[i], NULL, f, &i);
+        if (check_thread != 0)
             ft_error("failed to create thread");
         i++;
     }
-    return (philo_arr);
+    return (philo_id_arr);
 }
 
 void *philo_life(void *arg)
 {
-    t_philo *philo;
     int *id;
-    char *color;
 
-    id = (int *)arg;
-    color = "";
-    *id += 1;
-    philo = malloc(sizeof(t_philo));
-    if (philo == NULL)
-        ft_error("malloc failed (philo_life)");
-    philo->pthread_id = *id;
-    // ft_eating();
-    // ft_sleeping();
-    // ft_thinking();
+    id = (int *) arg;
     ft_log(id);
-
     return (NULL);
 }
 
@@ -62,18 +50,52 @@ void ft_log(int *philo_id)
 {
     char *color;
 
-    color = "";
+    color = log_format(philo_id);
+    ft_eating(philo_id, color);
+    ft_thinking(philo_id, color);
+    ft_sleeping(philo_id, color);
+}
+
+void    ft_eating(int *philo_id, char *color)
+{
+
+    printf("%s%i EAT"RESET"\n", color, *philo_id);
+}
+
+void    ft_thinking(int *philo_id, char *color)
+{
+    printf("%s%i THINK" RESET "\n", color, *philo_id);
+}
+
+void    ft_sleeping(int *philo_id, char *color)
+{
+    printf("%s%i SLEEP"RESET"\n", color, *philo_id);
+}
+
+char    *log_format(int *philo_id)
+{
+        char *color;
+        color = "";
     if (((*philo_id) % 1) == 0)
         color = PURPLE;
     if (((*philo_id) % 2) == 0)
         color = CYAN;
     if (((*philo_id) % 3) == 0)
         color = GREEN;
-
-    printf("%s%i EAT\n", color, *philo_id);
-    printf("%s%i SLEEP\n", color, *philo_id);
-    printf("%s%i THINK" RESET "\n\n", color, *philo_id);
+    return (color);
 }
 
-// void    ft_eating()
-// {}
+/* 
+
+int pthread_create(pthread_t *thread,
+                   const pthread_attr_t *attr,
+                   void *(*start_routine)(void *),
+                   void *arg);
+
+| Argument                         | Rôle                                       |
+| -------------------------------- | ------------------------------------------ |
+| `pthread_t *thread`              | Pointeur pour stocker l’ID du thread créé. |
+| `const pthread_attr_t *attr`     | Attributs du thread (NULL = défaut).       |
+| `void *(*start_routine)(void *)` | Fonction que le thread exécutera.          |
+| `void *arg`                      | Argument transmis à la fonction du thread. |
+*/
