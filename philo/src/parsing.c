7be4c7d2efd523b2e4e7c5d9865afcbef8823065
kobe <thodavid@student.42.fr>
2025-05-14 16:35:53 +0200

    ft_philo_arr_init

--- philo/src/parsing.c
+++ philo/src/parsing.c
@@ -6,15 +6,15 @@
 /*   By: thodavid <thodavid@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/29 15:01:30 by thodavid          #+#    #+#             */
-/*   Updated: 2025/05/01 14:43:29 by thodavid         ###   ########.fr       */
+/*   Updated: 2025/05/14 10:54:46 by thodavid         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../philo.h"
 
-t_data	ft_parsing(int ac, char **av)
+t_data	*ft_parsing(int ac, char **av)
 {
-	t_data	data;
+	t_data	*data;
 
 	if ((ac != 5) && (ac != 6))
 		ft_error("ARG ERR\n==HINT: 4 or 5 arguments==");
@@ -42,19 +42,24 @@ void	ft_check_args(char **av)
 	}
 }
 
-t_data	ft_stock_data(char **av)
+t_data	*ft_stock_data(char **av)
 {
-	t_data	table;
+	t_data	*table;
 
-	table.number_of_philos = ft_atoi(av[1]);
-	table.time_to_die = ft_atol(av[2]);
-	table.time_to_eat = ft_atol(av[3]);
-	table.time_to_sleep = ft_atol(av[4]);
+	table = malloc(sizeof(t_data));
+	if (table == NULL)
+		ft_error("table malloc failed");
+	
+	table->number_of_philos = ft_atoi(av[1]);
+	table->time_to_die = ft_atol(av[2]);
+	table->time_to_eat = ft_atol(av[3]);
+	table->time_to_sleep = ft_atol(av[4]);
+	table->philos_arr = ft_philo_arr_init(table->number_of_philos, table);
 	if (av[5] != NULL)
-		table.max_meal = ft_atoi(av[5]);
+		table->max_meal = ft_atoi(av[5]);
 	else
-		table.max_meal = 0;
-	ft_check_data(&table);
+		table->max_meal = 0;
+	ft_check_data(table);
 	return (table);
 }
 
@@ -68,7 +73,26 @@ void	ft_check_data(t_data *data)
 		ft_error("TIME TO EAT ERR\n==HINT: wrong time to eat==");
 	if (data->time_to_sleep > LONG_MAX || data->time_to_sleep < 1)
 		ft_error("TIME TO SLEEP ERR\n==HINT: wrong time to sleep==");
-	if (data->max_meal <= 0)
+	if (data->max_meal < 0)
 		ft_error("MAX MEAL ERR\n==HINT: wrong max meal==");
 }
 
+t_philo	*ft_philo_arr_init(int nbr_philo, t_data	*table)
+{
+	int		i;
+	t_philo	*philo_arr;
+	
+	i = 0;
+	philo_arr = malloc(sizeof(t_philo) * nbr_philo);
+	if (philo_arr == NULL)
+		ft_error("philo_arr malloc failed");
+	while (i < nbr_philo)
+	{
+		philo_arr[i].id = i + 1; 
+		philo_arr[i].right_fork = i;
+		philo_arr[i].right_fork = (i + 1) % nbr_philo;
+		philo_arr[i].data = table;
+		i++;
+	}
+	return (philo_arr);
+}
